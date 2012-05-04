27a6385d09bb025405465f7496982d158f82c545
k5t4j5 <boogs19.kj@gmail.com>
2012-05-04 10:15:56 +0100

    Add Theme Engine

--- config/common.mk
+++ config/common.mk
@@ -1,4 +1,7 @@
 # Supersu support
 PRODUCT_COPY_FILES += \
-    vendor/hybrid/prebuilt/common/bin/suinstaller.sh:system/bin/suinstaller.sh \
-    vendor/hybrid/prebuilt/common/supersu.zip:system/supersu.zip
+    vendor/hybrid/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+    vendor/hybrid/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+
+# T-Mobile theme engine
+include vendor/hybrid/config/themes_common.mk
